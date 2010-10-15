f30e3d417ad826373d57b58dabf569baafb295b9
David Workman <workmad3@gmail.com>
2010-10-15 11:52:25 +0100

    Finished off the editor plugin required to set up the TinyMCE stuff

--- spec/unit/spec.js
+++ spec/unit/spec.js
@@ -42,10 +42,64 @@ describe 'Editor Plugin'
         subject.init(editor, url);
       end
 
+      it 'should store the editor details'
+        subject.init(editor, url)
+        subject.editor.should.be editor
+      end
+
+      it 'should store the url details'
+        subject.init(editor, url)
+        subject.url.should.be url
+      end
+
       it 'should add a button to the editor'
         editor.should_receive("addButton").with_args("mysparql", button_options)
         subject.init(editor, url);
       end
+
+      describe 'command initialization'
+        before_each
+          editor.windowManager = {}
+          editor.windowManager.stub("open")
+          subject.editor = editor
+          subject.url = url
+        end
+
+        it 'should open a window'
+          window_properties = {file : url + '/dialog.htm', width : 100, height : 100, inline : 1}
+          plugin_url = {plugin_url : url}
+          editor.windowManager.should_receive("open").with_args(window_properties, plugin_url);
+          subject.addCommand()
+        end
+      end
+    end
+  end
+
+  describe 'control creation'
+    it 'should return null'
+      subject.createControl().should.be null
+    end
+  end
+
+  describe 'plugin info'
+    it 'should return an author'
+      subject.getInfo().author.should.be 'Hedtek Ltd.'
+    end
+
+    it 'should return the full plugin name'
+      subject.getInfo().longname.should.be 'TinyMCE MySparql Editor Plugin'
+    end
+
+    it 'should return the authors website url'
+      subject.getInfo().authorurl.should.be 'http://hedtek.com'
+    end
+
+    it 'should return a version'
+      subject.getInfo().version.should.be '0.1'
+    end
+
+    it 'should return a url for more info on the plugin'
+      subject.getInfo().infourl.should.be 'http://nymph.cs.man.ac.uk/projects/mysparql-tinymce'
     end
   end
 end
