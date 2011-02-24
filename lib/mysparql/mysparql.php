0f233198ca1b059ae4de471c72430a32941e10a0
David Workman <workmad3@gmail.com>
2011-02-24 16:43:06 +0000

    Updated to store the tinymce JS it requires and to have a WP plugin file. This allows the editor to be used in a WP install with only a small amount of faff

--- lib/mysparql/mysparql.php
+++ lib/mysparql/mysparql.php
@@ -1,3 +1,11 @@
+<?php
+/*
+Plugin Name: MySparql Editor
+Description: Allows the creation and editing of MySparql queries in wordpress
+Version: 0.1
+Author: David Workman
+*/
+
 class FD_MySparql_Editor {
    function FD_MySparql_Editor(){
     if(is_admin()){
@@ -6,6 +14,7 @@ class FD_MySparql_Editor {
            add_filter('tiny_mce_version', array(&$this, 'tiny_mce_version') );
            add_filter("mce_external_plugins", array(&$this, "mce_external_plugins"));
            add_filter('mce_buttons_2', array(&$this, 'mce_buttons'));
+           add_filter('tiny_mce_before_init', array(&$this, 'mce_options'));
         }
     }
    }
@@ -20,6 +29,20 @@ class FD_MySparql_Editor {
    function tiny_mce_version($version) {
     return ++$version;
    }
+   function mce_options($init) {
+     // Command separated string of extended elements
+     $ext = 'div[class|data-formatter|data-mysparql-id|data-xslt-type|class|data-parameterised|data-tutorial|data-google-viz|data-google-opts]';
+
+     // Add to extended_valid_elements if it alreay exists
+     if ( isset( $init['extended_valid_elements'] ) ) {
+       $init['extended_valid_elements'] .= ',' . $ext;
+     } else {
+       $init['extended_valid_elements'] = $ext;
+     }
+
+     // Super important: return $init!
+     return $init;
+   }
 }
  
 add_action('init', 'FD_MySparql_Editor');
@@ -27,3 +50,5 @@ function FD_MySparql_Editor(){
    global $FD_MySparql_Editor;
    $FD_MySparql_Editor = new FD_MySparql_Editor();
 }
+
+?>
